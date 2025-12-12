2dc080867f518a1096740f5b217d1a6f66621ad5
wu <wu@.com>
2025-12-12 11:23:04 +0800

    Build Test-kolotaev/vakt

--- setup.py
+++ setup.py
@@ -1,91 +1,76 @@
-#!/usr/bin/env python
-"""Wiggle Plot for Seismic Data Section
-
-Visualize seismic data section using wiggle plots.
-"""
-
-DOCLINES = __doc__.split("\n")
-
-import sys
-
 from setuptools import setup, find_packages
-
-# Check Python version
-if sys.version_info[:2] < (2, 7) or (3, 0) <= sys.version_info[0:2] < (3, 4):
-    raise RuntimeError("Python version 2.7 or >= 3.4 required.")
-
-CLASSIFIERS = """\
-Development Status :: 2 - Pre-Alpha
-Intended Audience :: Science/Research
-Intended Audience :: Developers
-License :: OSI Approved :: MIT License
-Programming Language :: Python
-Programming Language :: Python :: 2
-Programming Language :: Python :: 2.7
-Programming Language :: Python :: 3
-Programming Language :: Python :: 3.4
-Programming Language :: Python :: 3.5
-Programming Language :: Python :: 3.6
-Topic :: Software Development
-Topic :: Scientific/Engineering
-Operating System :: Microsoft :: Windows
-Operating System :: POSIX
-Operating System :: Unix
-Operating System :: MacOS
-
-"""
-
-MAJOR = 0
-MINOR = 1
-MICRO = 0
-ISRELEASED = False
-VERSION = '{}.{}.{}'.format(MAJOR, MINOR, MICRO)
+from os import path
 
 
-# VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+here = path.abspath(path.dirname(__file__))
+about = {}
+with open(path.join(here, 'vakt', 'version.py'), mode='r', encoding='utf-8') as f:
+    exec(f.read(), about)
 
-def setup_package():
-    # Figure out whether to add ``*_requires = ['numpy']``.
-    # We don't want to do that unconditionally, because we risk updating
-    # an installed numpy which fails too often.  Just if it's not installed, we
-    # may give it a try.  See gh-3379.
-    try:
-        import numpy
-    except ImportError:  # We do not have numpy installed
-        build_requires = ['numpy>=1.8.2']
-    else:
-        # If we're building a wheel, assume there already exist numpy wheels
-        # for this platform, so it is safe to add numpy to build requirements.
-        # See gh-5184.
-        build_requires = (['numpy>=1.8.2'] if 'bdist_wheel' in sys.argv[1:]
-                          else [])
-
-    try:
-        import matplotlib
-    except ImportError:
-        build_requires += ['matplotlib>=2.0.0']
-
-    metadata = dict(
-        name='wiggle',
-        maintainer="Lijun Zhu",
-        version=VERSION,
-        maintainer_email="gatechzhu@gmail.com",
-        description=DOCLINES[0],
-        long_description="\n".join(DOCLINES[2:]),
-        url="https://github.com/gatechzhu/wiggle",
-        download_url="https://github.com/gatechzhu/wiggle/releases",
-        license='MIT',
-        classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
-        platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
-        test_suite='tests',
-        packages=find_packages(),
-        setup_requires=build_requires,
-        install_requires=build_requires,
-        tests_require=build_requires + ['pytest'],
-    )
-
-    setup(**metadata)
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 if __name__ == '__main__':
-    setup_package()
+    setup(
+        name='vakt',
+        description='Attribute-based access control (ABAC) SDK for Python',
+        keywords='ACL ABAC access-control policy security authorization permission',
+        version=about['__version__'],
+        author='Egor Kolotaev',
+        author_email='ekolotaev@gmail.com',
+        license="Apache 2.0 license",
+        url='http://github.com/kolotaev/vakt',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        py_modules=['vakt'],
+        python_requires='>=3.4',
+        install_requires=[
+            'jsonpickle>=1.0',
+        ],
+        extras_require={
+            # Dev dependencies are not using pinned compatibility versions
+            # in order to support a wide array of Python versions
+            'dev': [
+                'pytest',
+                'pytest-cov',
+                'pylint',
+                'PyMySQL',
+                'psycopg2cffi~=2.8',
+            ],
+            'mongo': [
+                'pymongo~=3.5',
+            ],
+            'sql': [
+                'SQLAlchemy~=1.3',
+                # 'typing', # sqlalchemy imports typing since v1.3.24
+            ],
+            'redis': [
+                'redis~=3.3'
+            ],
+        },
+        packages=find_packages(exclude='tests'),
+        classifiers=[
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache Software License',
+            'Operating System :: OS Independent',
+            'Topic :: System :: Systems Administration',
+            'Topic :: System :: Networking',
+            'Topic :: System :: Networking :: Firewalls',
+            'Topic :: Security',
+            'Topic :: Software Development',
+            'Topic :: Utilities',
+            'Natural Language :: English',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
+            'Programming Language :: Python :: Implementation :: PyPy',
+            'Programming Language :: Python :: Implementation :: CPython',
+        ],
+    )
