404ac25122618818755e71cf65f03b810aa0a7cf
wu <wu@.com>
2025-12-12 11:58:01 +0800

    Build Test-dassh-dev/dassh

--- setup.py
+++ setup.py
@@ -1,26 +1,137 @@
-from setuptools import setup, find_packages
+########################################################################
+# Copyright 2021, UChicago Argonne, LLC
+#
+# Licensed under the BSD-3 License (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a
+# copy of the License at
+#
+#     https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+########################################################################
+"""
+date: 2022-01-20
+author: Milos Atz
+"""
+########################################################################
+import sys
+from setuptools import setup
+from setuptools._vendor.packaging import version
 
-setup(name = "artist",
-      version = "0.18.2",
-      packages = find_packages(),
-      url = "http://github.com/davidfokkema/artist/",
-      bugtrack_url='http://github.com/davidfokkema/artist/issues',
-      license='GPLv3',
-      author = "David Fokkema, Arne de Laat",
-      author_email = "davidfokkema@icloud.com, arne@delaat.net",
-      description = "A plotting library for Python with LaTeX output",
-      long_description=open('README.rst').read(),
-      keywords=['plots', 'plotting', 'data visualization'],
-      classifiers=['Intended Audience :: Science/Research',
-                   'Intended Audience :: Education',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3.4',
-                   'Topic :: Scientific/Engineering :: Visualization',
-                   'Topic :: Education',
-                   'Topic :: Text Processing :: Markup :: LaTeX',
-                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'],
-      install_requires = ['jinja2', 'numpy', 'Pillow'],
-      package_data={'artist': ['templates/*.tex']},
-)
+
+DESCRIPTION = "DASSH: Ducted Assembly Steady-State Heat Transfer software"
+LONG_DESCRIPTION = """DASSH performs steady-state thermal fluids
+calculations for hexagonal, ducted fuel assemblies typical of liquid
+metal fast reactors."""
+DISTNAME = 'DASSH'
+MAINTAINER = 'Milos Atz'
+MAINTAINER_EMAIL = 'matz@anl.gov'
+URL = 'https://github.com/dassh-dev/dassh'
+LICENSE = 'https://opensource.org/licenses/BSD-3-Clause'
+DOWNLOAD_URL = ''
+# FROM OPENMC: Get version information from __init__.py. This is ugly,
+# but more reliable than using an import.
+with open('dassh/__init__.py', 'r') as f:
+    VERSION = f.readlines()[-1].split()[-1].strip("'")
+
+
+###############################################################################
+
+
+def check_dependencies():
+    install_requires = []
+    # Just make sure dependencies exist, I haven't rigorously
+    # tested what are the minimal versions that will work
+    try:
+        import numpy
+        if sys.version_info < (3, 7):
+            assert version.parse(numpy.__version__) == version.parse('1.16.1')
+        else:
+            assert version.parse(numpy.__version__) >= version.parse('1.18')
+    except (ImportError, AssertionError):
+        install_requires.append('numpy == 1.16.1; python_version < "3.7"')
+        install_requires.append('numpy >= 1.18; python_version >= "3.7"')
+    try:
+        import matplotlib
+        assert version.parse(matplotlib.__version__) >= version.parse('3.2')
+    except (ImportError, AssertionError):
+        install_requires.append('matplotlib>=3.2')
+    try:
+        import configobj
+    except ImportError:
+        install_requires.append('configobj')
+    try:
+        import pandas
+    except ImportError:
+        install_requires.append('pandas')
+    try:
+        import pytest
+        if sys.version_info < (3, 7):
+            assert version.parse(pytest.__version__) == version.parse('5.4')
+    except (ImportError, AssertionError):
+        install_requires.append('pytest; python_version >= "3.7"')
+        install_requires.append('pytest == 5.4; python_version < "3.7"')
+    try:
+        import dill
+    except ImportError:
+        install_requires.append('dill')
+    return install_requires
+
+
+###############################################################################
+
+
+if __name__ == "__main__":
+    install_requires = check_dependencies()
+    # install_requires = [
+    #     'numpy == 1.16.1; python_version < "3.7"',
+    #     'numpy >= 1.18; python_version >= "3.7"',
+    #     'matplotlib>=3.2',
+    #     'configobj',
+    #     'pandas',
+    #     'pytest == 5.4; python_version < "3.7"',
+    #     'pytest; python_version >= "3.7"',
+    #     'dill'
+    # ]
+    setup(name=DISTNAME,
+          author=MAINTAINER,
+          author_email=MAINTAINER_EMAIL,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION,
+          license=LICENSE,
+          url=URL,
+          version=VERSION,
+          download_url=DOWNLOAD_URL,
+          python_requires='>=3.6',
+          install_requires=install_requires,
+          packages=['dassh'],
+          package_dir={'dassh': 'dassh'},
+          package_data={'dassh': ['./*.txt',
+                                  './correlations/*.py',
+                                  './_se2anl/*.py',
+                                  './py4c/*.py',
+                                  './varpow_osx.x',
+                                  './varpow_linux.x',
+                                  './data/*.csv',
+                                  'tests/*.py']},
+          include_package_data=True,
+          classifiers=['Intended Audience :: Science/Research',
+                       'Programming Language :: Python :: 3.6',
+                       'Programming Language :: Python :: 3.7+',
+                       'License :: OSI Approved :: BSD 3-Clause License',
+                       'Topic :: Scientific/Engineering :: Nuclear Energy',
+                       'Operating System :: Unix',
+                       'Operating System :: MacOS'],
+          entry_points={'console_scripts': [
+              'dassh = dassh.__main__:main',
+              'dassh_plot = dassh.__main__:plot']}
+          )
+
+
+###############################################################################
