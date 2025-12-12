f56b4bf1f77e97f6fc9a05b31e8c1a02ae0e3012
wu <wu@.com>
2025-12-12 10:57:48 +0800

    Build Test-tubular/sparkly

--- tests/no_extras/__init__.py
+++ tests/no_extras/__init__.py
@@ -13,6 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-include requirements.txt README.md
-recursive-include sparkly/resources *
