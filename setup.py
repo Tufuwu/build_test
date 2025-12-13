edd1968dc4b7af8d0efe41a6a3b83427eb270878
wu <wu@.com>
2025-12-13 16:51:04 +0800

    jakubplichta/grafana-dashboard-builder

--- setup.py
+++ setup.py
@@ -1,149 +1,103 @@
-# -*- coding: utf-8 -*
-"""
-Setup script for click-configfile.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2015-2020 grafana-dashboard-builder contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import unicode_literals
 
-USAGE:
-    python setup.py install
-"""
-
-from __future__ import absolute_import, print_function
 import sys
-import os.path
 
-HERE0 = os.path.dirname(__file__) or os.curdir
-os.chdir(HERE0)
-HERE = os.curdir
-sys.path.insert(0, HERE)
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
-from setuptools import find_packages, setup
-import inspect
+__author__ = 'Jakub Plichta <jakub.plichta@gmail.com>'
 
 
-# -----------------------------------------------------------------------------
-# UTILITY:
-# -----------------------------------------------------------------------------
-def find_packages_by_root_package(where):
-    """Better than excluding everything that is not needed,
-    collect only what is needed.
-    """
-    root_package = os.path.basename(where)
-    packages = [ "%s.%s" % (root_package, sub_package)
-                 for sub_package in find_packages(where)]
-    packages.insert(0, root_package)
-    return packages
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
 
-def make_long_description(marker=None, intro=None):
-    """
-    click_ is a framework to simplify writing composable commands for
-    command-line tools. This package extends the click_ functionality
-    by adding support for commands that use configuration files.
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
 
-    .. _click: https://click.pocoo.org/
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
 
-    EXAMPLE:
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
 
-    A configuration file, like:
-
-    .. code-block:: INI
-
-        # -- FILE: foo.ini
-        [foo]
-        flag = yes
-        name = Alice and Bob
-        numbers = 1 4 9 16 25
-        filenames = foo/xxx.txt
-            bar/baz/zzz.txt
-
-        [person.alice]
-        name = Alice
-        birthyear = 1995
-
-        [person.bob]
-        name = Bob
-        birthyear = 2001
-
-    can be processed with:
-
-    .. code-block:: python
-
-        # EXAMPLE:
-    """
-    if intro is None:
-        intro = inspect.getdoc(make_long_description)
-
-    with open("README.rst", "r") as infile:
-        line = infile.readline()
-        while not line.strip().startswith(marker):
-            line = infile.readline()
-
-        # -- COLLECT REMAINING: Usage example
-        contents = infile.read()
-
-    text = intro +"\n" + contents
-    return text
-
-# -- FILE: example_command_with_configfile.py (ALL PARTS: simplified)
-
-# ----------------------------------------------------------------------------
-# PROJECT CONFIGURATION (for sdist/setup mostly):
-# ----------------------------------------------------------------------------
-EXAMPLE_MARKER = "# MARKER-EXAMPLE:"
-long_description = make_long_description(EXAMPLE_MARKER)
-SETUP_DEBUG = os.environ.get("SETUP_DEBUG", "no") in ("yes", "true", "on")
-if SETUP_DEBUG:
-    print(long_description)
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
 
 
-# -----------------------------------------------------------------------------
-# SETUP:
-# -----------------------------------------------------------------------------
-setup(
-    name="click-configfile",
-    version="0.2.4",
-    url="https://github.com/click-contrib/click-configfile",
-    download_url="https://pypi.org/project/click-configfile/",
-    author="Jens Engel",
-    author_email="jenisys@noreply.github.com",
-    license="BSD",
-    description= "This package supports click commands that use configuration files.",
-    long_description = long_description,
-    keywords   = "click, configfile, configparser",
-    platforms  = [ 'any' ],
-    py_modules = ["click_configfile"],
-    # NOT_NEEDED: packages = find_packages_by_root_package("click_configfile"),
-    # -- REQUIREMENTS:
-    python_requires=">=2.7, !=3.0.*, !=3.1.*",
-    install_requires=[
-        "click >= 6.6",
-        "six >= 1.10",
-        "configparser >= 3.5.0; python_version < '3.5'",
+params = {
+    'name': 'grafana-dashboard-builder',
+    'version': '0.7.0a3',
+    'packages': [
+        'grafana_dashboards',
+        'grafana_dashboards.client',
+        'grafana_dashboards.components'
     ],
-    tests_require=[
-        "pytest <  5.0; python_version <  '3.0'", # >= 4.2
-        "pytest >= 5.0; python_version >= '3.0'",
-        "pytest-html >= 1.19.0",
+    'scripts': [
+        'bin/grafana_dashboard_builder.py'
     ],
-#     extras_require={
-#         # -- SUPPORT-WHEELS: Extra packages for Python2.6 and ...
-#         # SEE: https://bitbucket.org/pypa/wheel/ , CHANGES.txt (v0.24.0)
-#         ':python_version=="2.7"': "configparser >= 3.5.0; python_version < '3.5'",
-#         ':python_version=="3.3"': "configparser >= 3.5.0; python_version < '3.5'",
-#         ':python_version=="3.4"': "configparser >= 3.5.0; python_version < '3.5'",
-#     },
-    include_package_data=True,
-    zip_safe=True,
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: End Users/Desktop",
-        "Intended Audience :: System Administrators",
-        "Natural Language :: English",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Topic :: Utilities",
+    'url': 'https://github.com/jakubplichta/grafana-dashboard-builder',
+    'license': 'Apache License, Version 2.0',
+    'author': 'Jakub Plichta',
+    'author_email': 'jakub.plichta@gmail.com',
+    'description': 'Generate Grafana dashboards with YAML',
+    'classifiers': [
+        'Topic :: Utilities',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
-)
+    'keywords': 'grafana yaml graphite prometheus influxdb',
+    'cmdclass': {'test': Tox},
+    'tests_require': ['tox', 'mock'],
+    'install_requires': ['PyYAML>=5.3', 'argparse', 'requests-kerberos', 'requests'],
+    'python_requires': '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
+    'entry_points': {
+        'console_scripts': [
+            'grafana-dashboard-builder = grafana_dashboards.cli:main',
+        ],
+    },
+    'long_description':
+        """grafana-dashboard-builder is an open-source tool for easier creation of Grafana dashboards.
+It is written in Python and uses YAML descriptors for dashboard
+templates.
+
+This project has been inspired by Jenkins Job Builder that
+allows users to describe Jenkins jobs with human-readable format. grafana-dashboard-builder
+aims to provide similar simplicity to Grafana dashboard creation and to give users easy way how they can create
+dashboard templates filled with different configuration."""
+}
+
+setup(**params)
