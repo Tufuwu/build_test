141e924437ef5977148d136e42d5ec9316ac6afe
wu <wu@.com>
2025-12-13 13:16:38 +0800

    jpmckinney/validictory

--- setup.py
+++ setup.py
@@ -1,37 +1,39 @@
-import io
-import os
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-VERSION = '0.0.1'
+from validictory import __version__
 
-DESCRIPTION = (
-    'Flexible recursive directory iterator: scandir meets glob("**", recursive=True)'
-)
+with open('README.rst') as f:
+    long_description = f.read()
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-try:
-    with io.open(os.path.join(PROJECT_ROOT, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except IOError:
-    long_description = DESCRIPTION
-
-setup(
-    name='scantree',
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/andhus/scantree',
-    author="Anders Huss",
-    author_email="andhus@kth.se",
-    license='MIT',
-    install_requires=[
-        'attrs>=18.0.0',
-        'pathspec>=0.5.9'
-    ],
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    include_package_data=True,
-    entry_points={},
-    tests_require=['pytest', 'pytest-cov']
+setup(name='validictory',
+      version=__version__,
+      description='general purpose python data validator',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
+      author='James Turk',
+      author_email='james.p.turk@gmail.com',
+      url='http://github.com/jamesturk/validictory',
+      license='MIT',
+      classifiers=["Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: MIT License",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
+                   'Programming Language :: Python :: 3.10',
+                   'Programming Language :: Python :: 3.11',
+                   'Programming Language :: Python :: 3.12',
+                   'Programming Language :: Python :: Implementation :: CPython',
+                   'Programming Language :: Python :: Implementation :: PyPy',
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   ],
+      packages=find_packages(exclude=['validictory.tests', 'validictory.tests.*']),
+      extras_require={
+          'test': [
+              'pytest',
+              'pytest-cov',
+          ]
+      }
 )
