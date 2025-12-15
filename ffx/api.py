14c4a2af7e10e2cceb4eecb687eb19172d35d319
wu <wu@.com>
2025-12-15 10:56:20 +0800

    def fit(self, X, y):         X, y = self._validate_data(X, y, y_numeric=True)
            self.n_features_in_ = X.shape[1]
    
            if hasattr(X, 'columns'):
                varnames = None
            else:
                varnames = [f"X{i}" for i in range(X.shape[1])]
    
            self.models_ = run(X, y, X, y, varnames=varnames)
            self.model_ = self.models_[-1]
            return self

--- ffx/api.py
+++ ffx/api.py
@@ -17,19 +17,17 @@ class FFXRegressor(BaseEstimator, RegressorMixin):
     '''This class provides a Scikit-learn style estimator.'''
 
     def fit(self, X, y):
-        X, y = check_X_y(X, y, y_numeric=True, multi_output=False)
-        # if X is a Pandas DataFrame, we don't have to pass in varnames.
-        # otherwise we make up placeholders.
+        X, y = self._validate_data(X, y, y_numeric=True)
+
         self.n_features_in_ = X.shape[1]
-        
+
         if hasattr(X, 'columns'):
             varnames = None
         else:
-            varnames = ["X%d" % i for i in range(len(X))]
-        self.models_ = run(  # pylint: disable=attribute-defined-outside-init
-            X, y, X, y, varnames=varnames
-        )
-        self.model_ = self.models_[-1]  # pylint: disable=attribute-defined-outside-init
+            varnames = [f"X{i}" for i in range(X.shape[1])]
+
+        self.models_ = run(X, y, X, y, varnames=varnames)
+        self.model_ = self.models_[-1]
         return self
 
     def predict(self, X):
