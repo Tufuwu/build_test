d9481b94a6eecb0a91617823ccf3be40c9de720d
wu <wu@.com>
2025-12-15 21:59:26 +0800

    pohmelie/pytest-automock

--- otcustomizers/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='otcustomizers',
-    version='0.1',
-    author='Opentrons',
-    packages=find_packages(),
-    zip_safe=False
-)
--- protolib/__main__.py
+++
@@ -1,5 +0,0 @@
-from protolib import merge
-from protolib.traversals import PROTOCOLS_BUILD_DIR
-
-print(PROTOCOLS_BUILD_DIR)
-merge.merge_protocols(PROTOCOLS_BUILD_DIR)
