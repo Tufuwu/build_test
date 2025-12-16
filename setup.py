9f00d48411a95a31c55fa689850777d1bf60c63c
wu <wu@.com>
2025-12-16 15:39:30 +0800

    thegrill/grill

--- setup.py
+++ setup.py
@@ -1,24 +1,54 @@
-from setuptools import setup, find_packages
+import itertools
+from pathlib import Path
 
-version = __import__('shopify_webhook').__version__
+import setuptools
+from distutils.command.build import build
+from setuptools.command.develop import develop
+from setuptools.command.easy_install import easy_install
+from setuptools.command.install_lib import install_lib
 
-setup(
-    name = 'django-shopify-webhook',
-    version = version,
-    description = 'A package for the creation of Shopify Apps using the Embedded App SDK.',
-    long_description = open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author = 'Gavin Ballard',
-    author_email = 'gavin@discolabs.com',
-    url = 'https://github.com/discolabs/django-shopify-webhook',
-    license = 'None',
 
-    packages = find_packages(),
+# Hopefully all the following will not be needed after PEP 648
+# Reference: https://github.com/pytest-dev/pytest-cov/blob/daf54e79fcb8f549699d28e691302a9251f7e54b/setup.py#L145-L151
+def _copy_pth(obj, install_dir):
+    pth_src = Path(__file__).parent / "grill.pth"
+    pth_tgt = str(Path(install_dir) / pth_src.name)
+    obj.copy_file(str(pth_src), pth_tgt)
+    return [pth_tgt]
 
-    install_requires = [
-        'django >=1.7',
-    ],
 
-    zip_safe = True,
-    classifiers = [],
+class BuildPTH(build):
+    def run(self):
+        super().run()
+        _copy_pth(self, self.build_lib)
+
+
+class DevelopPTH(develop):
+    def run(self):
+        super().run()
+        _copy_pth(self, self.install_dir)
+
+
+class EasyInstallPTH(easy_install):
+    def run(self):
+        super().run()
+        _copy_pth(self, self.install_dir)
+
+
+class InstallLibPTH(install_lib):
+    def run(self):
+        super().run()
+        self.outputs = _copy_pth(self, self.install_dir)
+
+    def get_outputs(self):
+        return itertools.chain(super().get_outputs(), self.outputs)
+
+
+setuptools.setup(
+    cmdclass={
+        "build": BuildPTH,
+        "develop": DevelopPTH,
+        "easy_insall": EasyInstallPTH,
+        "install_lib": InstallLibPTH,
+    },
 )
