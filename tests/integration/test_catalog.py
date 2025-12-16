f215094bb494437207fff820395bd4be4afa35c2
wu <wu@.com>
2025-12-16 16:09:23 +0800

    tubular/sparkly

--- tests/integration/test_catalog.py
+++ tests/integration/test_catalog.py
@@ -15,7 +15,7 @@
 #
 
 from sparkly.testing import SparklyGlobalSessionTest
-from tests.integration.base import SparklyTestSession
+from tests.integration.base import SparklyTestSession, SparklyTestSessionWithOldCatalog
 from sparkly.catalog import read_db_properties_format
 
 
@@ -108,11 +108,11 @@ class TestSparklyCatalog(SparklyGlobalSessionTest):
         self.assertTrue(self.spark.catalog_ext.has_table('test_db.test_table'))
         self.assertFalse(self.spark.catalog_ext.has_table('test_db.new_test_table'))
 
-        self.spark.catalog_ext.rename_table('test_db.test_table', 'new_test_table')
+        self.spark.catalog_ext.rename_table('test_db.test_table', 'test_db.new_test_table')
 
         self.assertFalse(self.spark.catalog_ext.has_table('test_db.test_table'))
-        self.assertTrue(self.spark.catalog_ext.has_table('default.new_test_table'))
-        self.assertEqual(self.spark.table('default.new_test_table').count(), 2)
+        self.assertTrue(self.spark.catalog_ext.has_table('test_db.new_test_table'))
+        self.assertEqual(self.spark.table('test_db.new_test_table').count(), 2)
 
     def test_get_table_properties(self):
         properties = self.spark.catalog_ext.get_table_properties('test_table')
@@ -197,3 +197,7 @@ class TestSparklyCatalog(SparklyGlobalSessionTest):
 
         with self.assertRaises(ValueError):
             read_db_properties_format(')')
+
+
+class TestSparklyWithOldCatalog(TestSparklyCatalog):
+    session = SparklyTestSessionWithOldCatalog
