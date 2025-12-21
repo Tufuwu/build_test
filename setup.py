99acb370f9696c8e4d5fcf684fa84e5de2cee5de
wu <wu@.com>
2025-12-21 18:46:30 +0800

    roverdotcom/django-inlinecss

--- setup.py
+++ setup.py
@@ -1,69 +1,137 @@
-#/usr/bin/env python
-
 import os
 import sys
-from setuptools import setup, find_packages
+from shutil import rmtree
 
-# if you are not using vagrant, just delete os.link directly,
-# The hard link only saves a little disk space, so you should not care
-if os.environ.get('USER', '') == 'vagrant':
-    del os.link
+from setuptools import Command
+from setuptools import find_packages
+from setuptools import setup
+
+# Package meta-data.
+NAME = 'django-inlinecss'
+SRC_DIR = 'django_inlinecss'
+DESCRIPTION = 'A Django app useful for inlining CSS (primarily for e-mails)'
+URL = 'https://github.com/roverdotcom/django-inlinecss'
+EMAIL = 'philip@rover.com'
+AUTHOR = 'Philip Kimmey'
+REQUIRES_PYTHON = '>=3.8'
+VERSION = None
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'Django>=3.2',
+    'pynliner',
+    'future>=0.16.0',
+]
+
+# What packages are required only for tests?
+TESTS = [
+    'mock==5.1.0',
+    'pytest==8.0.1',
+    'pytest-django==4.8.0',
+]
+
+# What packages are optional?
+EXTRAS = {
+    'flake8': [
+        'flake8==7.0.0',
+        'flake8-isort==6.1.1',
+        'isort==5.13.2',
+        'testfixtures==8.0.0',
+    ],
+    "tests": TESTS,
+}
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 
-ROOT_DIR = os.path.dirname(__file__)
-SOURCE_DIR = os.path.join(ROOT_DIR)
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 
-if sys.version_info < (3, 6):
-    raise RuntimeError(
-        "opencage requires Python 3.7 or newer"
-        "Use older opencage 1.x for Python 2.7 or 3.6"
-    )
-
-# try for testing
 try:
-    with open(os.path.join(SOURCE_DIR, 'README.md'), encoding="utf-8") as f:
-        LONG_DESCRIPTION = f.read()
+    with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
-    LONG_DESCRIPTION = ""
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, SRC_DIR, "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print(f"\033[1m{s}\033[0m")
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system(f"{sys.executable} setup.py sdist bdist_wheel --universal")
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
+
 
 setup(
-    name="opencage",
-    version="2.2.0",
-    description="Wrapper module for the OpenCage Geocoder API",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    author="OpenCage GmbH",
-    author_email="info@opencagedata.com",
-    url="https://github.com/OpenCageData/python-opencage-geocoder/",
-    download_url="https://github.com/OpenCageData/python-opencage-geocoder/tarball/2.1.0",
-    license="BSD",
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    license="MIT",
+    url=URL,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    keywords=['geocoding', 'geocoder'],
+    keywords=["html", "css", "inline", "style", "email"],
     classifiers=[
-        'Environment :: Web Environment',
-        "Development Status :: 5 - Production/Stable",
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        "Programming Language :: Python :: 3 :: Only",
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Topic :: Scientific/Engineering :: GIS',
-        'Topic :: Utilities'
+        "Environment :: Other Environment",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Communications :: Email",
+        "Topic :: Text Processing :: Markup :: HTML",
     ],
-    install_requires=[
-        'Requests>=2.26.0',
-        'backoff>=1.10.0'
-    ],
-    test_suite='pytest',
-    tests_require=[
-        'httpretty>=0.9.6',
-        'pylint==2.15.9',
-        'pytest>=6.0'
-    ],
+    install_requires=REQUIRED,
+    tests_require=TESTS,
+    extras_require=EXTRAS,
+    # $ setup.py upload support.
+    cmdclass={
+        "upload": UploadCommand,
+    },
 )
