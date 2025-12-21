160e32d7ac2b61d0c4628a90f105c10f5fd86af2
wu <wu@.com>
2025-12-21 18:12:04 +0800

    jllorencetti/pytest-deadfixtures

--- setup.py
+++ setup.py
@@ -1,97 +1,75 @@
-# coding=utf-8
-# Copyright (c) 2016, Cedric Zhuang
-# All rights reserved.
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of disclaimer nor the names of its contributors may
-#       be used to endorse or promote products derived from this software
-#       without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS "AS IS" AND ANY
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-from setuptools import setup
-import io
-import re
+import codecs
 import os
+import re
 
-__author__ = 'Cedric Zhuang'
+from setuptools import Command, setup
 
-
-def version():
-    desc = get_long_description()
-    ret = re.findall(r'VERSION: (.*)', desc)[0]
-    return ret.strip()
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
-def here(filename=None):
-    ret = os.path.abspath(os.path.dirname(__file__))
-    if filename is not None:
-        ret = os.path.join(ret, filename)
-    return ret
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding="utf-8").read()
 
 
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n\n')
-    buf = []
-    for filename in filenames:
-        with io.open(here(filename), encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
+def get_version():
+    changes_path = os.path.join(BASE_PATH, "CHANGES.rst")
+    regex = r"^#*\s*(?P<version>[0-9]+\.[0-9]+(\.[0-9]+)?)$"
+    with codecs.open(changes_path, encoding="utf-8") as changes_file:
+        for line in changes_file:
+            res = re.match(regex, line)
+            if res:
+                return res.group("version")
+    return "0.0.0"
 
 
-def read_requirements(filename):
-    with open(filename) as f:
-        return f.read().splitlines()
+version = get_version()
 
 
-def get_long_description():
-    filename = 'README.md'
-    return read(filename)
+class VersionCommand(Command):
+    description = "print current library version"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        print(version)
 
 
 setup(
-    name="stockstats",
-    version=version(),
-    author="Cedric Zhuang",
-    author_email="jealous@163.com",
-    description="DataFrame with inline stock statistics support.",
-    license="BSD",
-    keywords="stock statistics indicator",
-    url="https://github.com/jealous/stockstats",
-    py_modules=['stockstats'],
-    platforms=['any'],
-    long_description=get_long_description(),
+    name="pytest-deadfixtures",
+    version=version,
+    author="João Luiz Lorencetti",
+    author_email="me@dirtycoder.net",
+    maintainer="João Luiz Lorencetti",
+    maintainer_email="me@dirtycoder.net",
+    license="MIT",
+    url="https://github.com/jllorencetti/pytest-deadfixtures",
+    description="A simple plugin to list unused fixtures in pytest",
+    long_description=read("README.rst"),
+    py_modules=["pytest_deadfixtures"],
+    install_requires=["pytest>=3.0.0"],
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Natural Language :: English",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Financial and Insurance Industry",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Operating System :: OS Independent",
-        "Development Status :: 4 - Beta",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: MIT License",
     ],
-    install_requires=read_requirements('requirements.txt'),
-    tests_require=read_requirements('test-requirements.txt'),
-    long_description_content_type='text/markdown',
+    cmdclass={"version": VersionCommand},
+    entry_points={"pytest11": ["deadfixtures = pytest_deadfixtures"]},
 )
