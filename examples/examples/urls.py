8a841f224d73748a10c7bae79d89d651c55cde35
wu <wu@.com>
2026-06-07 00:13:24 +0800

    sphinx-doc/sphinx-doc-translations

--- examples/examples/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-] + staticfiles_urlpatterns()
