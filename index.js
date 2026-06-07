f00a9a6bfb9c62f35919c73f161be59b0380ab41
wu <wu@.com>
2026-06-07 11:03:47 +0800

    unitech/pm2

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-'use strict'
+/**
+ * Copyright 2013-2022 the PM2 project authors. All rights reserved.
+ * Use of this source code is governed by a license that
+ * can be found in the LICENSE file.
+ */
 
-var path = require('path')
-var fs = require('fs')
+process.env.PM2_PROGRAMMATIC = 'true';
 
-exports.key = fs.readFileSync(path.join(__dirname, 'key.pem'))
-exports.cert = fs.readFileSync(path.join(__dirname, 'cert.pem'))
+var API = require('./lib/API.js');
+
+module.exports = new API;
+module.exports.custom = API;
